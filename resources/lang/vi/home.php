1758d331fd2d3f74eb4220983bdcd88d9c3c79f0
Quân Tôn <nhatquan160697@gmail.com>
2018-12-25 14:17:25 +0700

    Building layouts and views

---
+++ resources/lang/vi/home.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'title' => 'Laravel Quickstart - Nâng cao',
+    'lb_taskName' => 'Công việc',
+    'btn_addTask' => 'Thêm công việc',
+];
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [
+    'uses' => 'TaskController@index',
+    'as' => 'tasks'
+]);
